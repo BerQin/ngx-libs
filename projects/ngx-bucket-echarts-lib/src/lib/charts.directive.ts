694b92c2a91fdf831b23436dad8614d65be3d969
BerQin <berqin@outlook.com>
2020-06-24 16:25:12 +0800

    fix: 提交关于组件的打印问题

--- projects/ngx-bucket-echarts-lib/src/lib/charts.directive.ts
+++ projects/ngx-bucket-echarts-lib/src/lib/charts.directive.ts
@@ -29,7 +29,6 @@ export class ChartsDirective implements OnInit, OnDestroy, OnChanges, AfterViewI
   public _Options: Chart;
   @Input() set options(value: Chart) {
     this._Options = this.chartService.creatOption(value);
-    console.log(this._Options);
   }
 
   get options(): Chart {
